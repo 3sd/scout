4242ac0dff60071c198c334ccde646e8bc7fa239
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-07-04 11:11:28 +0100

    create a no check mode for installation which is useful for copying and pasting commands

--- src/Command/InstallInstanceCommand.php
+++ src/Command/InstallInstanceCommand.php
@@ -16,6 +16,7 @@ class InstallInstanceCommand extends ScoutCommand
         ->setName('install-instance')
 
         ->addOption('domain', null, InputOption::VALUE_REQUIRED, "Domain for this instance (defaults to http://<project-name>)")
+        ->addOption('no-checks', 'N', InputOption::VALUE_NONE, "Don't check requirements. Also sets --dry-run")
 
         ->addProjectOption()
 
@@ -29,67 +30,74 @@ class InstallInstanceCommand extends ScoutCommand
         if(!$input->getOption('domain')){
             $input->setOption('domain', "{$this->name}");
         }
+        if($input->getOption('no-checks')){
+            $input->setOption('dry-run', true);
+        }
     }
 
     protected function prepare(InputInterface $input, OutputInterface $output)
     {
+
         // A variable to decide whether we should install CiviCRM
         $this->installCiviCRM = realpath("{$this->path}/sites/all/modules/civicrm");
 
-
-        // Check that settings files do not exist
-        $vhostFilesToCreate = ["/etc/nginx/sites-enabled/{$this->name}", "/etc/nginx/sites-available/{$this->name}"];
-        $existingvhostFiles = [];
-
-        foreach($vhostFilesToCreate as $vhostFile){
-            if(realpath($vhostFile)){
-                $existingvhostFiles[] = $vhostFile;
-            }
-        }
-        if(count($existingvhostFiles) == 1){
-            throw new \Exception( "Cannot proceed with install. The following vhost already exists: ".implode(', ', $existingvhostFiles).'.');
-        }elseif(count($existingvhostFiles) > 1){
-            throw new \Exception( "Cannot proceed with install. The following vhosts already exist: ".implode(', ', $existingvhostFiles).'.');
-        }
-
-        $settingsFilesToCreate = ["{$this->path}/sites/default/civicrm.settings.php", "{$this->path}/sites/default/settings.php"];
-        $existingSettingsFiles = [];
-
-        foreach($settingsFilesToCreate as $settingFile){
-            if(realpath($settingFile)){
-                $existingSettingsFiles[] = $settingFile;
-            }
-        }
-        if(count($existingSettingsFiles) == 1){
-            throw new \Exception( "Cannot proceed with install. The following settings file already exists: ".implode(', ', $existingSettingsFiles).'.');
-        }elseif(count($existingSettingsFiles) > 1){
-            throw new \Exception( "Cannot proceed with install. The following settings files already exist: ".implode(', ', $existingSettingsFiles).'.');
-        }
-
-        if($this->installCiviCRM){
-            $civiCRMExtensionsDir = "{$this->path}/sites/all/civicrm_extensions";
-            if(!realpath($civiCRMExtensionsDir)){
-                throw new \Exception( "Cannot proceed with install. CiviCRM extensions directory ($civiCRMExtensionsDir) does not exist.");
-            }
-        }
-
-        // Check that the databases do not already exist
         $dbsToCreate[] = "{$this->dbName}_drupal";
         if($this->installCiviCRM){
             $dbsToCreate[] = "{$this->dbName}_civicrm";
         }
-        $existingDbs = [];
-        $mysql = $this->getContainer()->get('mysql');
-        $result = $mysql->query("SHOW DATABASES LIKE '{$this->dbName}_%'");
-        foreach($result->fetch_all() as $db){
-            if(in_array($db[0], $dbsToCreate)){
-                $existingDbs[] = $db[0];
+
+        if(!$input->getOption('no-checks')){
+
+            // Check that settings files do not exist
+            $vhostFilesToCreate = ["/etc/nginx/sites-enabled/{$this->name}", "/etc/nginx/sites-available/{$this->name}"];
+            $existingvhostFiles = [];
+
+            foreach($vhostFilesToCreate as $vhostFile){
+                if(realpath($vhostFile)){
+                    $existingvhostFiles[] = $vhostFile;
+                }
             }
-        }
-        if(count($existingDbs) == 1){
-            throw new \Exception( "Cannot proceed with install. The following database already exists: ".implode(', ', $existingDbs).'.');
-        }elseif(count($existingDbs) > 1){
-            throw new \Exception( "Cannot proceed with install. The following databases already exist: ".implode(', ', $existingDbs).'.');
+            if(count($existingvhostFiles) == 1){
+                throw new \Exception( "Cannot proceed with install. The following vhost already exists: ".implode(', ', $existingvhostFiles).'.');
+            }elseif(count($existingvhostFiles) > 1){
+                throw new \Exception( "Cannot proceed with install. The following vhosts already exist: ".implode(', ', $existingvhostFiles).'.');
+            }
+
+            $settingsFilesToCreate = ["{$this->path}/sites/default/civicrm.settings.php", "{$this->path}/sites/default/settings.php"];
+            $existingSettingsFiles = [];
+
+            foreach($settingsFilesToCreate as $settingFile){
+                if(realpath($settingFile)){
+                    $existingSettingsFiles[] = $settingFile;
+                }
+            }
+            if(count($existingSettingsFiles) == 1){
+                throw new \Exception( "Cannot proceed with install. The following settings file already exists: ".implode(', ', $existingSettingsFiles).'.');
+            }elseif(count($existingSettingsFiles) > 1){
+                throw new \Exception( "Cannot proceed with install. The following settings files already exist: ".implode(', ', $existingSettingsFiles).'.');
+            }
+
+            if($this->installCiviCRM){
+                $civiCRMExtensionsDir = "{$this->path}/sites/all/civicrm_extensions";
+                if(!realpath($civiCRMExtensionsDir)){
+                    throw new \Exception( "Cannot proceed with install. CiviCRM extensions directory ($civiCRMExtensionsDir) does not exist.");
+                }
+            }
+
+            // Check that the databases do not already exist
+            $existingDbs = [];
+            $mysql = $this->getContainer()->get('mysql');
+            $result = $mysql->query("SHOW DATABASES LIKE '{$this->dbName}_%'");
+            foreach($result->fetch_all() as $db){
+                if(in_array($db[0], $dbsToCreate)){
+                    $existingDbs[] = $db[0];
+                }
+            }
+            if(count($existingDbs) == 1){
+                throw new \Exception( "Cannot proceed with install. The following database already exists: ".implode(', ', $existingDbs).'.');
+            }elseif(count($existingDbs) > 1){
+                throw new \Exception( "Cannot proceed with install. The following databases already exist: ".implode(', ', $existingDbs).'.');
+            }
         }
 
         $twig = $this->getContainer()->get('twig');
